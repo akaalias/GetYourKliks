1a043f2bcf86a21e7497f80908328599f702ae7c
Alexis Rondeau <alexisrondeau@Alexiss-MacBook-Air.local>
2012-01-21 17:14:17 -0500

    Created Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetyourkliksCom::Application.config.secret_token = '43f9fa6a683a5306e17ea6829970cbaf1645cdf6096bb281f1d0ec890aa8a9e73d15bdbf82ea591c3e32723410278117a4967a1c6a648d75f76e806916897406'
